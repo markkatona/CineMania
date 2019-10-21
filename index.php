7dc3cd3ac69c2bdd47b3dcd83471fa5eac68d874
robicsirak99 <robi.csirak99@gmail.com>
2019-10-21 14:59:50 +0200

    index.php módosítás: javascript hozzáadása a menühöz.

--- index.php
+++ index.php
@@ -2,7 +2,44 @@
 <html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<script>
 
+	function myFunc(id){
+		switch (id){
+			case "HOME" : {
+				console.log("home");
+				document.getElementById("content").innerHTML = "HOME";
+				break;
+			}
+			case "FILMEK" : {
+				console.log("filmek");
+				document.getElementById("content").innerHTML = "FILMEK";
+				break;
+			}
+			case "ERTEKELES" : {
+				console.log("ertekeles");
+				document.getElementById("content").innerHTML = "ERTEKELES";
+				break;
+			}
+			case "ARAINK" : {
+				console.log("araink");
+				document.getElementById("content").innerHTML = "ARAINK";
+				break;
+			}
+			case "CINEBAR" : {
+				console.log("cinebar");
+				document.getElementById("content").innerHTML = "CINEBAR";
+				break;
+			}
+			case "ELERHETOSEGEK" : {
+				console.log("elerhetosegek");
+				document.getElementById("content").innerHTML = "ELERHETOSEGEK";
+				break;
+			}
+		}
+	}
+
+</script>
 </head>
 <body>
 
