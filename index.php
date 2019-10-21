0944d6a479991cde4bdd3b73aa1b2c9de57fca2f
robicsirak99 <robi.csirak99@gmail.com>
2019-10-21 14:58:04 +0200

    index.php fáj feltöltése: főmenü vázának elkészítése.

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+</head>
+<body>
+
+<div class="topnav">
+  <a class="active" href="javascript:myFunc('HOME')">HOME/LOGO</a>
+  <a href="javascript:myFunc('FILMEK')">FILMEK</a>
+  <a href="javascript:myFunc('ERTEKELES')">ERTEKELES</a>
+  <a href="javascript:myFunc('ARAINK')">ARAINK</a>
+  <a href="javascript:myFunc('CINEBAR')">CINEBAR</a>
+  <a href="javascript:myFunc('ELERHETOSEGEK')">ELERHETOSEGEK</a>
+</div>
+
+<div id="content" class="content">
+  <h2>HOME</h2>
+</div>
+
+</body>
+</html>
