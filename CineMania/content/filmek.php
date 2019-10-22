d5a8ec223cd3471de70f8d1a1e9468fc1c7856dc
istvan <bucsistvan99@gmail.com>
2019-10-22 12:47:27 +0200

    A filmek.php fájlban a calendar függvény megírása, mely meghatározza a jelenlegi dátumot.

--- CineMania/content/filmek.php
+++ CineMania/content/filmek.php
@@ -2,59 +2,14 @@
 <html>
 <head>
 	<style type="text/css">
-		* {
-		  box-sizing: border-box;
-		}
-		.column1 {
-		  float: left;
-		  width: 10%;
-		  margin: 1em;
-		  height: 12em;
-		  background-color: powderblue;
-		  border: 0.1em solid green;
-		}
-		.column2 {
-		  float: left;
-		  margin: 1em;
-		  height: 12em;
-		  background-color: powderblue;
-		  border: 0.1em solid green;
-		  width: 85%;
-		}
-		.column1 img {
-			height: 10em;
-			width: 80%;
-			margin: 0.75em;
-		}
-		.kartyak:after {
-		  content: "";
-		  display: table;
-		  clear: both;
-		}
-		.kartyak {
-		  border: 0.1em solid gray;
-		}
-		.column2 p {
-		  float: left;
-		  width: 20%;
-		  text-align: center;
-		  padding-top: 3.5em;
-		}
+		
 	</style>
 </head>
 <body>
 
-<div class="kartyak">
-	<div class="column1">
-		<img src="filmkepek/Avengers-Endgame.jpg">
-	</div>
-	<div class="column2">
-		<p>szinkron</p>
-		<p>felirat</p>
-		<p>tipusa</p>
-		<p>terem</p>
-		<p>vetités</p>
-	</div>
+<div>
+	<img src="filmkepek/Avengers-Endgame.jpg">
+	<p>Bosszúállók Végjáték</p>
 </div>
 
 </body>
