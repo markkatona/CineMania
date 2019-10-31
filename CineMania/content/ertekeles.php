04fcdb6a0df47010538268fc4bacd631693f6f5c
robicsirak99 <robi.csirak99@gmail.com>
2019-10-31 16:40:45 +0200

    ertekeles.php-ban javascript és css fájlok beolvasása külső fájlként

--- CineMania/content/ertekeles.php
+++ CineMania/content/ertekeles.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html>
 <html>
-<body>
-
-ertekeles
-
-</body>
+	<body>
+		<script src="ertekeles/scripts.js"></script>
+		<style src="ertekels/style.css"></style>
+		
+		<div class="bg-image"></div>
+		<div class="content"></div>
+	</body>
 </html>
