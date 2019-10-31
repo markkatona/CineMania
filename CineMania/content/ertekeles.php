2ae0bb42808ded3908d0ab5128a320db34b0b288
robicsirak99 <robi.csirak99@gmail.com>
2019-10-31 16:48:20 +0200

    ertekeles.php fájlban háttérkép bugfix, és css külső fájként beolvasás bugfix

--- CineMania/content/ertekeles.php
+++ CineMania/content/ertekeles.php
@@ -1,8 +1,11 @@
 <!DOCTYPE html>
 <html>
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="ertekeles/style.css">
+	</head>
 	<body>
-		<script src="ertekeles/scripts.js"></script>
-		<style src="ertekels/style.css"></style>
+		<script src="ertekeles/script.js"></script>
 		
 		<div class="bg-image"></div>
 		<div class="content"></div>
