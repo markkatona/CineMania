8a8f0805a401699c6f9d054ecab801ecc27e5a51
robicsirak99 <robi.csirak99@gmail.com>
2019-11-17 20:20:17 +0100

    ertekeles.php oldalon az adatbázisból kiolvasott adat háttér elé helyezése

--- CineMania/content/ertekeles.php
+++ CineMania/content/ertekeles.php
@@ -5,23 +5,24 @@
 		<link rel="stylesheet" type="text/css" href="ertekeles/style.css">
 	</head>
 	<body>
-		<?php
-			$conn = new mysqli("localhost", "root", "", "cinemania");
-			if ($conn->connect_error) {
-			die("Connection failed: " . $conn->connect_error);
-			}
-			$sql = "select film_id, cim from filmek ";
-			$result = $conn->query($sql);
-			echo "<table border=1> <tr> <th>Városnév</th><th>Lakosság</th> </tr>" ;
-			while ($eredmeny= mysqli_fetch_row($result) )
-			{
-			echo "<tr><td>" . $eredmeny[0] . "</td> <td>" . $eredmeny[1] . "</th></tr>" ;
-			}
-			echo "</table>" ;
-		?>
 		<script src="ertekeles/script.js"></script>
 		
 		<div class="bg-image"></div>
-		<div class="content"></div>
+		<div class="content">
+
+			<?php
+				$conn = new mysqli("localhost", "root", "", "cinemania");
+				if ($conn->connect_error) {
+				die("Connection failed: " . $conn->connect_error);
+				}
+				$sql = "select film_id, cim from filmek ";
+				$result = $conn->query($sql);
+				while ($eredmeny= mysqli_fetch_row($result) ){
+					echo "<div>" . $eredmeny[0] . $eredmeny[1] ."</div>";
+				}
+				echo "</table>" ;
+			?>
+			
+		</div>
 	</body>
 </html>
