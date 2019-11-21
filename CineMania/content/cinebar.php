86f50325d3725fbd41549475b783a2493d6d5c59
robicsirak99 <robi.csirak99@gmail.com>
2019-11-21 23:27:20 +0200

    cinebar.php alap felepites hozzaadasa

--- CineMania/content/cinebar.php
+++ CineMania/content/cinebar.php
@@ -1,8 +1,54 @@
 <!DOCTYPE html>
 <html>
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="ertekeles/style.css?version=3">
+	</head>
 <body>
-
-cinebar
-
+	<div>
+		<div> szoveg </div>
+		<div> hely </div>
+		<div> gomb </div>
+	</div>
+	<div>
+		<div> 
+			<div> popcorn szoveg </div>
+			<div> popcorn nagy kep</div>
+			<div> 
+				<div> kiskep1 </div>
+				<div> kiskep2</div>
+			</div>
+			<div> 
+				<div> kiskep3 </div>
+				<div> kiskep4</div>
+			</div>
+		</div>
+		<div>
+			<div>
+				<div>kep1</div>
+				<div>kep2</div>
+			</div>
+			<div>
+				<div>kep3</div>
+				<div>kep4</div>
+			</div>
+			<div>
+				<div>kep5</div>
+				<div>kep6</div>
+			</div>
+		</div>
+		<div> 
+			<div> nacho szoveg </div>
+			<div> nacho nagy kep</div>
+			<div> 
+				<div> kiskep1 </div>
+				<div> kiskep2</div>
+			</div>
+			<div> 
+				<div> kiskep3 </div>
+				<div> kiskep4</div>
+			</div>
+		</div>
+	</div>
 </body>
 </html>
