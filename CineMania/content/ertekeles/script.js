4232caca58f528ec72824cb96fd58718ff0bbed0
robicsirak99 <robi.csirak99@gmail.com>
2019-11-21 12:55:34 +0200

    ertekeles oldalhoz tartozo script.js fajlban a filmehez eventListener hozzaadas

--- CineMania/content/ertekeles/script.js
+++ CineMania/content/ertekeles/script.js
@@ -1,4 +1,21 @@
-function clicked(){
-	console.log("asd");
-	//call php function with ajax here
+var film_count;
+
+function functionnn(n){
+	film_count = n;
+	console.log("YAY");
+	console.log(film_count);
 }
+
+window.onload = function() {
+	for (i = 0; i < film_count; i++) {
+		var id = "link".concat(String(i));
+		console.log(id);
+		document.getElementById(id).addEventListener('click', doSomething);
+	}
+	function doSomething() {
+		console.log("asd");
+		document.getElementById("myH2").style.display = "none";
+	}
+	
+}
+
