9630ce947b23afa48367419c92717781e581070f
robicsirak99 <robi.csirak99@gmail.com>
2019-11-22 13:00:04 +0200

    cinebar.php oldal elkeszitese a hozza tartozo style.css-el egyutt

--- CineMania/content/cinebar.php
+++ CineMania/content/cinebar.php
@@ -2,52 +2,52 @@
 <html>
 	<head>
 		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="stylesheet" type="text/css" href="cinebar/style.css?version=1">
 	</head>
 <body>
+	<div class="bg-image"></div>
 	<div class="content">
 		<div class="submitcode">
 			<div id="submittext"> Töltsd fel a vásárláskor kapott kódot és nyerj fantasztikus ajándékokat! </div>
 			<div id="submitinput"> <input type="text" name="fname"><br> </div>
-			<div id="submitbutton"> <input type="submit" value="Submit"> </div>
+			<div id="submitbutton"> <a href="#" id="button" class="button">BEKÜLD</a> </div>
 		</div>
 		<div class="products">
 			<div class="popcorns"> 
-				<div id="popcorn_text"> popcorn szoveg </div>
-				<div id="popcorn_bigpic"> popcorn nagy kep</div>
+				<div id="popcorn_text"> Diákoknak 10% kedvezmény! </div>
+				<div id="popcorn_bigpic"></div>
 				<div class="small_pic_row"> 
-					<div id="popcorn_smallpic_1"> kiskep1 </div>
-					<div id="popcorn_smallpic_2"> kiskep2 </div>
+					<div id="popcorn_smallpic_1"></div>
+					<div id="popcorn_smallpic_2"></div>
 				</div>
 				<div class="small_pic_row"> 
-					<div id="popcorn_smallpic_3"> kiskep3 </div>
-					<div id="popcorn_smallpic_4"> kiskep4</div>
+					<div id="popcorn_smallpic_3"></div>
+					<div id="popcorn_smallpic_4"></div>
 				</div>
 			</div>
 			<div class="drinks">
 				<div class="drinks_row_1">
-					<div id="drink_1">kep1</div>
-					<div id="drink_2">kep2</div>
+					<div id="drink_1"></div>
+					<div id="drink_2"></div>
+					<div id="drink_3"></div>
 				</div>
 				<div class="drinks_row_2">
-					<div id="drink_3">kep3</div>
-					<div id="drink_4">kep4</div>
-				</div>
-				<div class="drinks_row_3">
-					<div id="drink_5">kep5</div>
-					<div id="drink_6">kep6</div>
+					<div id="drink_4"></div>
+					<div id="drink_5"></div>
+					<div id="drink_6"></div>
 				</div>
 			</div>
 			<div class="nachos"> 
-				<div id="nacho_text"> nacho szoveg </div>
-				<div id="nacho_big_pic"> nacho nagy kep</div>
-				<div class="small_pic_row1"> 
-					<div id="nacho_smallpic_1"> kiskep1 </div>
-					<div id="nacho_smallpic_2"> kiskep2</div>
+				<div id="nacho_text"> Diákoknak 10% kedvezmény! </div>
+				<div id="nacho_bigpic"></div>
+				<div class="small_pic_row"> 
+					<div id="nacho_smallpic_1"></div>
+					<div id="nacho_smallpic_2"></div>
 				</div>
 				<div class="small_pic_row"> 
-					<div id="nacho_smallpic_3"> kiskep3 </div>
-					<div id="nacho_smallpic_4"> kiskep4</div>
+					<div id="nacho_smallpic_3"></div>
+					<div id="nacho_smallpic_4"></div>
 				</div>
 			</div>
 		</div>
