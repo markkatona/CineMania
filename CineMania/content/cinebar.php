9ca9ebe94034e8b6b379a70ef32c7aab799aa6e1
robicsirak99 <robi.csirak99@gmail.com>
2019-11-22 11:13:01 +0200

    cinebar.php oldalon az alap felepiteshez classok es idk hozzaadasa

--- CineMania/content/cinebar.php
+++ CineMania/content/cinebar.php
@@ -2,51 +2,53 @@
 <html>
 	<head>
 		<meta charset="UTF-8">
-		<link rel="stylesheet" type="text/css" href="ertekeles/style.css?version=3">
+		<link rel="stylesheet" type="text/css" href="cinebar/style.css?version=1">
 	</head>
 <body>
-	<div>
-		<div> szoveg </div>
-		<div> hely </div>
-		<div> gomb </div>
-	</div>
-	<div>
-		<div> 
-			<div> popcorn szoveg </div>
-			<div> popcorn nagy kep</div>
-			<div> 
-				<div> kiskep1 </div>
-				<div> kiskep2</div>
-			</div>
-			<div> 
-				<div> kiskep3 </div>
-				<div> kiskep4</div>
-			</div>
+	<div class="content">
+		<div class="submitcode">
+			<div id="submittext"> Töltsd fel a vásárláskor kapott kódot és nyerj fantasztikus ajándékokat! </div>
+			<div id="submitinput"> <input type="text" name="fname"><br> </div>
+			<div id="submitbutton"> <input type="submit" value="Submit"> </div>
 		</div>
-		<div>
-			<div>
-				<div>kep1</div>
-				<div>kep2</div>
+		<div class="products">
+			<div class="popcorns"> 
+				<div id="popcorn_text"> popcorn szoveg </div>
+				<div id="popcorn_bigpic"> popcorn nagy kep</div>
+				<div class="small_pic_row"> 
+					<div id="popcorn_smallpic_1"> kiskep1 </div>
+					<div id="popcorn_smallpic_2"> kiskep2 </div>
+				</div>
+				<div class="small_pic_row"> 
+					<div id="popcorn_smallpic_3"> kiskep3 </div>
+					<div id="popcorn_smallpic_4"> kiskep4</div>
+				</div>
 			</div>
-			<div>
-				<div>kep3</div>
-				<div>kep4</div>
+			<div class="drinks">
+				<div class="drinks_row_1">
+					<div id="drink_1">kep1</div>
+					<div id="drink_2">kep2</div>
+				</div>
+				<div class="drinks_row_2">
+					<div id="drink_3">kep3</div>
+					<div id="drink_4">kep4</div>
+				</div>
+				<div class="drinks_row_3">
+					<div id="drink_5">kep5</div>
+					<div id="drink_6">kep6</div>
+				</div>
 			</div>
-			<div>
-				<div>kep5</div>
-				<div>kep6</div>
-			</div>
-		</div>
-		<div> 
-			<div> nacho szoveg </div>
-			<div> nacho nagy kep</div>
-			<div> 
-				<div> kiskep1 </div>
-				<div> kiskep2</div>
-			</div>
-			<div> 
-				<div> kiskep3 </div>
-				<div> kiskep4</div>
+			<div class="nachos"> 
+				<div id="nacho_text"> nacho szoveg </div>
+				<div id="nacho_big_pic"> nacho nagy kep</div>
+				<div class="small_pic_row1"> 
+					<div id="nacho_smallpic_1"> kiskep1 </div>
+					<div id="nacho_smallpic_2"> kiskep2</div>
+				</div>
+				<div class="small_pic_row"> 
+					<div id="nacho_smallpic_3"> kiskep3 </div>
+					<div id="nacho_smallpic_4"> kiskep4</div>
+				</div>
 			</div>
 		</div>
 	</div>
