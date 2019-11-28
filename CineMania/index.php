5a130cc5d75454c25e1e1b9a5ca65a2e4deed8ac
Katona Mark <m.katona@yahoo.com>
2019-11-28 20:44:51 +0100

    index.php Fejléc designjának megváltoztatása és a regisztráció előkészítése.

--- CineMania/index.php
+++ CineMania/index.php
@@ -1,3 +1,16 @@
+<?php
+  session_start();
+
+  if (!isset($_SESSION['username'])) {
+    $_SESSION['msg'] = "You must log in first";
+    header('location: content/registration/login.php');
+  }
+  if (isset($_GET['logout'])) {
+    session_destroy();
+    unset($_SESSION['username']);
+    header("location: content/registration/login.php");
+  }
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -17,7 +30,7 @@
 
 </script>
 <style>
- 
+
 html, body {
 	overflow: auto;
 	margin: 0;
@@ -26,15 +39,18 @@ html, body {
 
 .topnav {
 	overflow: hidden;
-	background-color: #00a3cc;
+	background-color: #575757;
 }
 
 #menu{
+	border-bottom: 0.2em solid #404040;
+	border-bottom-left-radius: 20px 10px;
+	border-bottom-right-radius: 20px 10px;
     height: 7vh;
     width: 100%;
     display: grid;
     grid-template-rows: 100%;
-    grid-template-columns: 20% 16% 16% 16% 16% 16%;
+    grid-template-columns: 20% 15% 15% 15% 15% 20%;
     grid-template-areas: "B0 B1 B2 B3 B4 B5";
     position: fixed;
 	z-index: 10;
@@ -44,7 +60,7 @@ html, body {
 #button0{grid-area: B0;}
 #button1{grid-area: B1;}
 #button2{grid-area: B2;}
-#button3{grid-area: B3;}    
+#button3{grid-area: B3;}
 #button4{grid-area: B4;}
 #button5{grid-area: B5;}
 
@@ -53,19 +69,29 @@ div.topnav > a {
     align-items: center;
     justify-content: center;
     width: 100%;
-    height: 100%; 
+    height: 100%;
     color: white;
     text-decoration: none;
     font-size: large;
 }
+div.content{
+	position: fixed;
+	top: 2.5em;
+	z-index: 10;
+    opacity: 0.95;
+}
+div.content > p{
+	float: left;
+	margin-left: 2em;
+}
 
 .topnav a:hover {
-	background-color: #33d6ff;
+	background-color: #bc161b;
 	color: white;
 }
 
 .topnav a.active {
-	background-color: #33d6ff;
+	background-color: #bc161b;
 	color: white;
 }
 
@@ -89,7 +115,29 @@ iframe {
 	  <a id="button5" href="javascript:loadContent('button5','elerhetosegek.php')">ELÉRHETŐSÉGEK</a>
 	</div>
 
+	<div class="content">
+	  	<!-- notification message -->
+	  	<!--<?php if (isset($_SESSION['success'])) : ?>
+	      <div class="error success" >
+	      	<h3>
+	          <?php
+	          	echo $_SESSION['success'];
+	          	unset($_SESSION['success']);
+	          ?>
+	      	</h3>
+	      </div>
+	  	<?php endif ?>-->
+
+	    <!-- logged in user information -->
+	    <?php  if (isset($_SESSION['username'])) : ?>
+	    	<p>Üdvözöljük, <strong><?php echo $_SESSION['username']; ?></strong></p>
+	    	<p class="logout"> <a href="content/registration/login.php?logout='1'" style="color: red;">logout</a> </p>
+	    <?php endif ?>
+	</div>
+
 	<iframe id="myiframe" src="content/home.php" frameborder="0" height="100%" width="100%"></iframe>
+
+
 
 </body>
 </html>
